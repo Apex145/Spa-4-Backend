33282025cbf9175062117142b0e020fd44a8fb5c
joel25dx <xjustelle@gmail.com>
2026-04-22 11:58:42 +0200

    added mappings

--- src/main/java/com/SPA_4/demo/repositories/BookingRepository.java
+++ src/main/java/com/SPA_4/demo/repositories/BookingRepository.java
@@ -1,5 +1,9 @@
 package com.SPA_4.demo.repositories;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
@@ -7,6 +11,8 @@ import org.springframework.stereotype.Repository;
 import com.SPA_4.demo.Database.BookingDB;
 
 @Repository
-public interface BookingRepository extends MongoRepository<BookingDB, String>{
-    
+public interface BookingRepository extends MongoRepository<BookingDB, String> {
+
+    List<BookingDB> findByDate(String date);
+
 }
